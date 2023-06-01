93b1081bc7d8dbab1d92b59fb68774cdf2d0a282
vishnugowtham97 <vishnugowtham97@gmail.com>
2023-06-01 10:40:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./Routes/AuthRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/jwt", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", authRoutes);
